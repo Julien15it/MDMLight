e22549910da91e6c75a317f555c60f820931ec31
Arthur Geeraert <arthur.geeraert@alluvion.eu>
2026-08-20 12:07:37 +0000

    add ui5 form

---
+++ app/businesspartner/webapp/controller/App.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator"
+    ],
+    function(BaseController, Filter, FilterOperator) {
+      "use strict";})
